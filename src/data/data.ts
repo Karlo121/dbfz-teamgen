e4db120dceec1679340e893f4760ea0d03a7e262
Karlo <51051819+Karlo121@users.noreply.github.com>
2021-03-06 23:01:24 +0100

    added characters

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,38 +1,181 @@
-import android16img from "../img/android16.png";
-import android17img from "../img/android17.png";
-import android18img from "../img/android18.png";
-import android21img from "../img/android21.png";
-import beerusImg from "../img/beerus.png";
-import ginyuImg from "../img/ginyu.png";
-import cellImg from "../img/cell.png";
+import android16 from "../img/android16.png";
+import android17 from "../img/android17.png";
+import android18 from "../img/android18.png";
+import android21 from "../img/android21.png";
+import beerus from "../img/beerus.png";
+import ginyu from "../img/ginyu.png";
+import cell from "../img/cell.png";
+import frieza from "../img/frieza.png";
+import teenGohan from "../img/teengohan.png";
+import adultGohan from "../img/adultgohan.png";
+import ssGoku from "../img/ssgoku.png";
+import ssgssGoku from "../img/ssgssgoku.png";
+import blackGoku from "../img/gokublack.png";
+import gotenks from "../img/gotenks.png";
+import hit from "../img/hit.png";
+import kidBuu from "../img/kidbuu.png";
+import krillin from "../img/krillin.png";
+import majinBuu from "../img/majinbuu.png";
+import nappa from "../img/nappa.png";
+import piccolo from "../img/piccolo.png";
+import tien from "../img/tien.png";
+import trunks from "../img/trunks.png";
+import ssVegeta from "../img/ssvegeta.png";
+import ssgssVegeta from "../img/ssgssvegeta.png";
+import yamcha from "../img/yamcha.png";
+import bardock from "../img/bardock.png";
+import broly from "../img/broly.png";
+import cooler from "../img/cooler.png";
+import goku from "../img/goku.png";
+import vegeta from "../img/vegeta.png";
+import vegito from "../img/vegito.png";
+import zamasu from "../img/zamasu.png";
+import gokuGt from "../img/gokugt.png";
+import gokuUltraInstinct from "../img/gokuultrainstinct.png";
 
 export const characters = [
   {
     name: "Android 16",
-    img: android16img,
+    img: android16,
   },
   {
     name: "Android 17",
-    img: android17img,
+    img: android17,
   },
   {
     name: "Android 18",
-    img: android18img,
+    img: android18,
   },
   {
     name: "Android 21",
-    img: android21img,
+    img: android21,
   },
   {
     name: "Beerus",
-    img: beerusImg,
+    img: beerus,
   },
   {
     name: "Captain Ginyuu",
-    img: ginyuImg,
+    img: ginyu,
   },
   {
     name: "Cell",
-    img: cellImg,
+    img: cell,
+  },
+  {
+    name: "Frieza",
+    img: frieza,
+  },
+  {
+    name: "Teen Gohan",
+    img: teenGohan,
+  },
+  {
+    name: "Adult Gohan",
+    img: adultGohan,
+  },
+  {
+    name: "Adult Gohan",
+    img: adultGohan,
+  },
+  {
+    name: "SS Goku",
+    img: ssGoku,
+  },
+  {
+    name: "SSGSS Goku",
+    img: ssgssGoku,
+  },
+  {
+    name: "Black Goku",
+    img: blackGoku,
+  },
+  {
+    name: "Gotenks",
+    img: gotenks,
+  },
+  {
+    name: "Hit",
+    img: hit,
+  },
+  {
+    name: "Kid Buu",
+    img: kidBuu,
+  },
+  {
+    name: "Krillin",
+    img: krillin,
+  },
+  {
+    name: "Majin Buu",
+    img: majinBuu,
+  },
+  {
+    name: "Nappa",
+    img: nappa,
+  },
+  {
+    name: "Piccolo",
+    img: piccolo,
+  },
+  {
+    name: "Tien",
+    img: tien,
+  },
+  {
+    name: "Trunks",
+    img: trunks,
+  },
+  {
+    name: "SS Vegeta",
+    img: ssVegeta,
+  },
+  {
+    name: "SS Vegeta",
+    img: ssVegeta,
+  },
+  {
+    name: "SSGSS Vegeta",
+    img: ssgssVegeta,
+  },
+  {
+    name: "Yamcha",
+    img: yamcha,
+  },
+  {
+    name: "Bardock",
+    img: bardock,
+  },
+  {
+    name: "Broly",
+    img: broly,
+  },
+  {
+    name: "Cooler",
+    img: cooler,
+  },
+  {
+    name: "Goku",
+    img: goku,
+  },
+  {
+    name: "Vegeta",
+    img: vegeta,
+  },
+  {
+    name: "Vegito",
+    img: vegito,
+  },
+  {
+    name: "Zamasu",
+    img: zamasu,
+  },
+  {
+    name: "Goku GT",
+    img: gokuGt,
+  },
+  {
+    name: "Goku Ultra Instinct",
+    img: gokuUltraInstinct,
   },
 ];
