e7d39cbe2b2a01aa15c74b4bea561d6195f22048
Karlo <51051819+Karlo121@users.noreply.github.com>
2021-03-07 14:58:58 +0100

    update

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,5 +1,7 @@
 import { StyledDiv } from '../styles/Character.style';
+import { StyledButton } from '../styles/Material.style';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 interface CharProps {
   name: string;
@@ -24,6 +26,7 @@ const Character: React.FC<Props> = (props: Props) => {
   };
 
   const populateArray = () => {
+    charsToPlay = [];
     while (charsToPlay.length < 3) {
       addElement();
     }
@@ -35,23 +38,37 @@ const Character: React.FC<Props> = (props: Props) => {
   return (
     <Grid
       container
-      spacing={2}
-      direction='row-reverse'
-      justify='space-evenly'
+      spacing={3}
+      direction='column'
       alignItems='center'
+      justify='space-evenly'
     >
-      <StyledDiv>
-        <img src={charsToPlay[0].img} height='150px' alt='firstChar' />
-        <p>{charsToPlay[0].name}</p>
-      </StyledDiv>
-      <StyledDiv>
-        <img src={charsToPlay[1].img} height='150px' alt='secondChar' />
-        <p>{charsToPlay[1].name}</p>
-      </StyledDiv>
-      <StyledDiv>
-        <img src={charsToPlay[2].img} height='150px' alt='thirdChar' />
-        <p>{charsToPlay[2].name}</p>
-      </StyledDiv>
+      <Grid item xs={12} sm={12} alignItems='stretch'>
+        <StyledButton>Generate</StyledButton>
+      </Grid>
+
+      <Grid container xs={12} direction='row-reverse' justify='space-between'>
+        <Grid item xs={4} md={4}>
+          <StyledDiv>
+            <img src={charsToPlay[0].img} height='150px' alt='firstChar' />
+            <p>{charsToPlay[0].name}</p>
+          </StyledDiv>
+        </Grid>
+
+        <Grid item xs={4} md={4}>
+          <StyledDiv>
+            <img src={charsToPlay[1].img} height='150px' alt='firstChar' />
+            <p>{charsToPlay[1].name}</p>
+          </StyledDiv>
+        </Grid>
+
+        <Grid item xs={4} md={4}>
+          <StyledDiv>
+            <img src={charsToPlay[2].img} height='150px' alt='firstChar' />
+            <p>{charsToPlay[2].name}</p>
+          </StyledDiv>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
--- src/styles/Character.style.ts
+++ src/styles/Character.style.ts
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
-  margin-top: 20px;
   padding: 5px;
-  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   background-color: white;
-  width: 150px;
   text-align: center;
   img {
     display: block;
